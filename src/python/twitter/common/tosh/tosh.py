2d5e1fe7cf767860be1f3d9554d4f5ce80b50e23
Suman Karumuri <skarumuri@twitter.com>
2013-12-29 00:50:12 -0800

    Added a class called tosh.

--- src/python/twitter/common/tosh/tosh.py
+++ src/python/twitter/common/tosh/tosh.py
@@ -2,12 +2,11 @@ import sqlparse
 
 print sqlparse.parse('select * from foo;')
 
-def run():
- while (1):
-   x = raw_input('>')
-   print "input is: " + x
+class ToShell:
+    def start(self):
+      while (1):
+        x = raw_input('>')
+        print "input is: " + x
 
-def start():
-  run()
-
-start()
+tosh = ToShell()
+tosh.start()
