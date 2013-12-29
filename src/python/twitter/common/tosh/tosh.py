8eaa9037ed25b4bde7b2572371720d84166514ea
Suman Karumuri <skarumuri@twitter.com>
2013-12-29 01:04:45 -0800

    Added a command parser that prints tokens.

--- src/python/twitter/common/tosh/tosh.py
+++ src/python/twitter/common/tosh/tosh.py
@@ -1,12 +1,22 @@
 import sqlparse
 
-print sqlparse.parse('select * from foo;')
+class CommandParser:
+  "Command parser for tosh."
+  def parse(self, command):
+    result = sqlparse.parse(command)
+    stmt = result[0]
+    return stmt.tokens
 
-class ToShell:
-    def start(self):
-      while (1):
-        x = raw_input('>')
-        print "input is: " + x
+class TableOrientedShell:
+  "Table Oriented shell (tosh) lets you access the system using a table oriented interface."
+  def __init__(self):
+    self.cmd_parser = CommandParser()
 
-tosh = ToShell()
+  def start(self):
+    while (1):
+      command = raw_input('>')
+      print "input is: " + command
+      print self.cmd_parser.parse(command)
+
+tosh = TableOrientedShell()
 tosh.start()
